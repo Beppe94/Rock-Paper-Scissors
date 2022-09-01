c09978f30b445022e394f23ea1e1bd132a678f71
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-09-01 21:43:46 +0200

    user input logic and test function

--- logic.js
+++ logic.js
@@ -1,15 +1,40 @@
 //computer choice
-function getcomputerchoice(options) {
+function getcomputerchoice() {
     let choices = ['rock', 'paper', 'scissors']
     const random = (Math.floor(Math.random() * choices.length))
     return choices[random]
 }
 
+
 //user choice
 function getuserchoice() {
-    let input = prompt('choose between: rock, paper, scissors: ');
-    input.toLowerCase()
-    return input
+    let input = ''
+    while ((input != 'rock') || (input != 'paper') || (input != 'scissors')) {
+        input = prompt('choose between: Rock, Paper, Scissors ').toLowerCase()
+        if (input == 'rock' || input == 'paper' || input == 'scissors')
+            return input
+        else {
+            return 'Invalid input'
+        }
+    }   
 }
 
-getuserchoice()
+function round(computer, person) {
+    if (computer == 'rock' && person == 'scissors') {
+        return 'The computer wins!! Try again!'
+    } else if (computer == 'paper' && person == 'rock') {
+        return 'The computer wins!! Try again!'
+    } else if (computer == 'scissors' && person == 'paper') {
+        return 'The computer wins!! Try again!'
+    } else if (computer == 'rock' && person == 'rock'){
+        return 'Tie'
+    } else if (computer == 'paper' && person == 'paper'){
+        return 'Tie'
+    } else if (computer == 'scissors' && person == 'scissors'){
+        return 'Tie'
+    } else {
+        return 'You win!!'
+    }
+}
+
+console.log(round(getcomputerchoice(), getuserchoice()))
